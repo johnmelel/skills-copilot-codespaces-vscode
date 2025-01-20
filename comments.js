6949d1f02ad6d329a051d3e94efb17fc88a0b18f
John Melel <84776601+johnmelel@users.noreply.github.com>
2025-01-20 02:15:06 +0000

    comments aaa

---
+++ comments.js
@@ -0,0 +1,25 @@
+//create web server
+const express = require('express');
+const app = express();
+//create server
+const http = require('http');
+const server = http.createServer(app);
+//create socket.io
+const socketIo = require('socket.io');
+const io = socketIo(server);
+//create array for comments
+let comments = [];
+//create event for connection
+io.on('connection', (socket) => {
+  //send comments to client
+  socket.emit('comments', comments);
+  //receive comments from client
+  socket.on('comment', (comment) => {
+    comments.push(comment);
+    io.emit('comments', comments);
+  });
+});
+//listen port
+server.listen(3000, () => {
+  console.log('Server running');
+});
